aaad13ef2fe701e9f777f5a0ad25963569ed98bf
diegojsanchez <76917258+diegojsanchez@users.noreply.github.com>
2021-12-02 09:47:11 +0100

    Add files via upload

---
+++ All Elements at Level_DSG.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#BIBLIOTECAS
+import clr 
+clr.AddReference('RevitAPI') 
+from Autodesk.Revit.DB import FilteredElementCollector, ElementLevelFilter
+clr.AddReference('RevitServices')
+from RevitServices.Persistence import DocumentManager
+doc = DocumentManager.Instance.CurrentDBDocument 
+
+#FUNCIONES
+def todos_elementos_por_nivel(n):
+	'''Uso: Colecta todos los elementos por nivel.'''
+	filtro = ElementLevelFilter(n.Id)
+	elementos = FilteredElementCollector(doc).WherePasses(filtro).WhereElementIsNotElementType().ToElements()
+	return elementos
+
+#ENTRADAS
+nivel = UnwrapElement(IN[0]) #Para los elementos que vienen de Dynamo
+#SALIDA
+OUT = todos_elementos_por_nivel(nivel)
