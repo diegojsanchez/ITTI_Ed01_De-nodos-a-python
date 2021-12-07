d50e62f4f8f694a7c129abb38598bfa4561a00fc
diegojsanchez <76917258+diegojsanchez@users.noreply.github.com>
2021-12-07 08:43:47 +0100

    todas_instancias_por_nivel

--- Revit_Selection_All Elements at Level_DSG.py
+++ Revit_Selection_All Elements at Level_DSG.py
@@ -9,7 +9,7 @@ from RevitServices.Persistence import DocumentManager
 doc = DocumentManager.Instance.CurrentDBDocument 
 
 #FUNCIONES
-def todos_elementos_por_nivel(n):
+def todas_instancias_por_nivel(n):
 	'''Uso: Colecta todos los elementos por nivel.'''
 	filtro = ElementLevelFilter(n.Id)
 	elementos = FilteredElementCollector(doc).WherePasses(filtro).WhereElementIsNotElementType().ToElements()
